d7eeb3c4b8f1578a4bb7946ec8e1c39984837f33
hanz94 <hansikam94@gmail.com>
2025-01-31 10:24:16 -0500

    Guest context table modeled

--- verification/formal/sev/rosette/tables.rkt
+++ verification/formal/sev/rosette/tables.rkt
@@ -1,69 +1,26 @@
-; TDX tables and configurations
 #lang rosette
-(require data/bit-vector)   ; bitvectors
-(require "tdx_lib.rkt")     ; various state defintions and helper functions
 
-;********************* Tables *********************
+;; Guest Context Table: Maps guest handles to their state and encryption properties
+(define GCTX (make-hash))
 
-; See p56 for description of key management tables for confidentiality
+(define/contract GCTX-contract
+  (hash/c integer? (list/c symbol? integer? integer? integer? integer? integer? integer? integer?))
+  GCTX)
 
-; GPA : guest physical address, HPA : host physical address
+;; Add a new guest entry to the GCTX table
+(define (add-guest handle state asid policy vek tek tik nonce ms)
+  (hash-set! GCTX handle (list state asid policy vek tek tik nonce ms)))
 
-; (S)EPT -> maps GPA to HPA and page state (shared bit, EPT entry state)
-(define secure_EPT (make-hash))
-(define-struct secure_EPT_entry (host_physical_address GPA_SHARED state))
-(define/contract secure_EPT-contract
-    (hash/c integer? secure_EPT_entry? #:flat? #t)
-    secure_EPT)
+;; Retrieve a guest entry from GCTX
+(define (get-guest handle)
+  (hash-ref GCTX handle #f))
 
-; KET[HKID] -> TD's ephemeral encryption key
-(define KET (make-hash))
-(define/contract KET-contract
-    (hash/c integer? bitvector? #:flat? #t)
-    KET)
-
-; KOT[HKID] -> State (free, assigned, reclaimed, flushed, n/a (hkid free), reserved)
-; See table 4.3 in TDX specification for state table, table 4.1 for KET and KOT
-(define KOT (make-hash))
-(define/contract KOT-contract
-    (hash/c integer? integer? #:flat? #t)
-    KOT)
-
-; TDR : Trust Domain Root
-; See table 23.3 on p.171
-(define-struct TDR (INIT FATAL NUM_TDCX TDCX_PA CHLDCNT LIFECYCLE_STATE HKID PKG_CONFIG_BITMAP FINALIZED RUNNING))
+;; Update guest state in GCTX
+(define (update-guest-state handle new-state)
+  (when (hash-has-key? GCTX handle)
+    (define guest (hash-ref GCTX handle))
+    (hash-set! GCTX handle (list new-state (cadr guest) (caddr guest) (cadddr guest)
+                                 (list-ref guest 4) (list-ref guest 5) (list-ref guest 6)
+                                 (list-ref guest 7) (list-ref guest 8)))))
 
 
-; TDCS : Trust Domain Control Structure
-; See table 23.5 on p.172 (All the fields are not implemented here for simplicity)
-(define-struct TDCS (FINALIZED NUM_VCPUS NUM_ASSOC_VCPUS ATTRIBUTES EPTP TSC_OFFSET TSC_MULTIPLIER))
-
-
-; TDVPS : Trust Domain Virtual Process State
-; See table 23.9 on p.176 (All the fields are not implemented here for simplicity)
-(define-struct TDVPS (VCPU_STATE LAUNCHED VCPU_INDEX NUM_TDVPX TDVPS_PAGE_PA ASSOC_LPID ASSOC_HKID VCPU_EPOCH CPUID_SUPERVISOR_VE))
-
-
-; PAMT (p67-68)
-; Note that the OWNER field is a bit field from the pa of the TDR, here it
-; could probably be some TD_ID instead
-; EPOCH is only intended for pages of type PT_REG or PT_EPT
-; PAMTs are more complex than this because there are multiple page sizes but
-; I don't think that'll impact confidentiality.
-(define PAMT (make-hash))
-(define-struct PAMT_entry (PAGE_TYPE OWNER BEPOCH))
-(define/contract PAMT-contract
-    (hash/c integer? PAMT_entry? #:flat? #t)
-    PAMT)
-
-; Need some way to model what is in the cache at any given time so that we can model
-; flushing the cache during context switches or TD teardown. So I think a map of like
-; physical addresses to a cache line structure, where the data field doesn't matter but
-; we need to have the TD owner bit and HKID at the very least. Maybe the MAC too
-
-; So on p120 it specifies the components for Ci mode, as the data, a tweaked pa, the TD owner bit, and the MAC key
-(define cache (make-hash))
-(define-struct cache_entry (TD_OWNER HKID MAC DATA))
-(define/contract cache-contract
-    (hash/c integer? cache_entry? #:flat? #t)
-    cache)
